47aa10124f7d743b966e5cdaf9f8111c6f60da6c
DESKTOP-R9JAUM9\keizman <home_fire@163.com>
2025-04-09 21:20:19 +0800

    Revert "i18n"
    This reverts commit 86cc413f5d7cc6586fb93de319af1a1bc338c455.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -35,7 +35,7 @@ const config: Config = {
   organizationName: 'facebook', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
 
-  onBrokenLinks: 'warn',
+  onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/intro">
+            to="/docs/intro">
             Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
